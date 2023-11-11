9fbad1a74e9c09733ea5540604132b6abef76a02
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-12 00:46:02 +0100

    Update 050-requests.py

--- 050-requests.py
+++ 050-requests.py
@@ -1,1 +1,10 @@
-# pypi.org/projects/requests
+# pypi.org/project/requests
+# itunes
+
+import requests
+import sys
+if len(sys.argv) != 2:
+    sys.exit
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
